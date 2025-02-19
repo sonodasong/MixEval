6d5479a692f2d2f541a0ef5a26d023d30f2c438f
null <sonodasong@gmail.com>
2025-02-19 14:20:40 +0800

    Add llama 3.2 support

--- mix_eval/models/__init__.py
+++ mix_eval/models/__init__.py
@@ -9,6 +9,8 @@ AVAILABLE_MODELS = {
     "llama_3_8b_instruct": "Llama_3_8B_Instruct",
     "llama_3_70b": "Llama_3_70B",
     "llama_3_70b_instruct": "Llama_3_70B_Instruct",
+    "llama_32_1b_instruct": "Llama_32_1B_Instruct",
+    "llama_32_3b_instruct": "Llama_32_3B_Instruct",
     
     "qwen_15_4b": "Qwen_15_4B",
     "qwen_15_7b": "Qwen_15_7B",
